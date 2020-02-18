e8f0372fd08d4f69bc7b47bd45a08e448c4338ab
Andrés Mendoza <daniel.mendo04@gmail.com>
2020-02-18 14:11:07 -0500

    Layouts Fibonacci

---
+++ app/src/main/java/com/example/fibonacci/fib_seq.java
@@ -0,0 +1,14 @@
+package com.example.fibonacci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fib_seq extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fib_seq);
+    }
+}
